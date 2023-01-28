8a55d6b95a1ecbf48a6c405d52319de70f5a01ab
Anna Kuznetsova <a.kuznetsova@nlogic.ai>
2023-01-28 06:13:38 +0300

    add answer options

--- convert_clevr_to_visdial.py
+++ convert_clevr_to_visdial.py
@@ -2,6 +2,7 @@ import json
 import spacy
 import tqdm
 import transformers
+import random
 
 from coreference import extract_clusters
 
@@ -9,7 +10,7 @@ nlp = spacy.load("en_core_web_sm")
 tokenizer = transformers.BertTokenizer.from_pretrained("bert-base-uncased")
 
 
-def main(clevr_path, save_path):
+def main(clevr_path, save_path, mix_dialogs=3):
     clevr = json.load(open(clevr_path))
     images = json.load(open('clevr/images.json'))
     images = [i[2:-1] for i in images]
@@ -21,7 +22,7 @@ def main(clevr_path, save_path):
         image_ix = 'CLEVR_val_' + '0' * (6 - len(image_ix)) + image_ix
         if image_ix not in images:
             continue
-        for dialog in dialog_set['dialogs'][:1]:
+        for dialog in dialog_set['dialogs']:
             new_rounds = []
             dialog_tokenized = []
             caption = tokenizer.tokenize(dialog['caption'])
@@ -42,17 +43,34 @@ def main(clevr_path, save_path):
                 answer_ix = clevr_answers.index(answer)
                 new_rounds.append({
                     'answer': answer_ix,
-                    'question': question_ix
+                    'question': question_ix,
+                    'answer_options': list(range(100)),
+                    "gt_index": answer_ix,
                 })
             clusters = extract_clusters(dialog)
-            new_dialog = {
-                'image_id': image_ix,
-                'dialog': new_rounds,
-                'caption': caption,
-                'clusters': clusters,
-                'round_id': len(new_rounds),
-            }
-            clevr_dialogs.append(new_dialog)
+            if mix_dialogs > 0:
+                ixs = random.choices(range(len(new_rounds)), k=mix_dialogs)
+                ixs += [len(new_rounds)]
+                new_dialogs = [
+                    {
+                        'image_id': image_ix,
+                        'dialog': new_rounds[:i],
+                        'caption': caption,
+                        'clusters': clusters,
+                        'round_id': i,
+                    }
+                    for i in ixs
+                ]
+                clevr_dialogs.extend(new_dialogs)
+            else:
+                new_dialog = {
+                    'image_id': image_ix,
+                    'dialog': new_rounds,
+                    'caption': caption,
+                    'clusters': clusters,
+                    'round_id': len(new_rounds),
+                }
+                clevr_dialogs.append(new_dialog)
     clevr_visdial = {
         'data': {
             'dialogs': clevr_dialogs,
@@ -60,11 +78,12 @@ def main(clevr_path, save_path):
             'questions': clevr_questions
         },
         'split': 'test',
-        'version': '1.1'
+        'version': '1.2'
     }
     json.dump(clevr_visdial, open(save_path, 'w'))
 
 
 if __name__ == '__main__':
     main(clevr_path='clevr/CLEVR_VD_VAL.json',
-         save_path='clevr/CLEVR_VD_VAL_VISDIAL_1_dialog_per_1000_pictures.json')
+         save_path='clevr/CLEVR_VD_VAL_VISDIAL_1000_pictures_full_dialogs.json',
+         mix_dialogs=0)
